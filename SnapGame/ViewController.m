bcf174a7a504edf245faa8fbc2d96a82ee0438cd
Jason Williams <jasonmw_nf@hotmail.com>
2016-01-27 04:01:40 -0330

    created UI

--- SnapGame/ViewController.m
+++ SnapGame/ViewController.m
@@ -17,6 +17,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    scoreInt = 0;
+    timerInt = 20;
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +28,24 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)startGame:(id)sender {
+    
+    if (scoreInt == 0) {
+        timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(TimerUpdate) userInfo:nil repeats:YES];
+    }
+}
+
+-(void)TimerUpdate {
+    
+    timerInt -= 1;
+    
+    self.timerLabel.text = [NSString stringWithFormat:@"%i", timerInt];
+    
+    if (timerInt == 0) {
+        [timer invalidate];
+    }
+}
+
+- (IBAction)snap:(id)sender {
+}
 @end
